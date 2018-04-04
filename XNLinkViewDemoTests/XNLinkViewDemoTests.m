99ce1f2bdab7d24a175f2e32c8fe7f5ce299593a
zhangbinbin <zhangbinbin@xiaoniu.com>
2018-04-04 14:54:46 +0800

    demo 添加

---
+++ XNLinkViewDemoTests/XNLinkViewDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  XNLinkViewDemoTests.m
+//  XNLinkViewDemoTests
+//
+//  Created by 张彬彬 on 2018/4/4.
+//  Copyright © 2018年 张彬彬. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XNLinkViewDemoTests : XCTestCase
+
+@end
+
+@implementation XNLinkViewDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
